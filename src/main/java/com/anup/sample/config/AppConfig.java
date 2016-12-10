585da97b824f0ece6e910763bd69af529db90515
Sreelekshmi <Sreelekshmi>
2016-12-10 16:56:16 -0500

    First commit

---
+++ src/main/java/com/anup/sample/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.anup.sample.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.anup.sample")
+class AppConfig {
+}
